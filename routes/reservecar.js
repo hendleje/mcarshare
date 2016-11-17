0c499f3d5e56f114f5a37dcf8652be531e7733c2
hendleje <jenny2912@gmx.de>
2016-11-17 14:13:04 -0330

    copied .js files
    application.js and reservecar.js are now in the routes folder

---
+++ routes/reservecar.js
@@ -0,0 +1,20 @@
+function reserveCar() {
+	var customer1 = new Customer("Jenny", "Hendler", "bla", "bla");
+
+	// If the user has paid the previous Bill then he is directed to the
+	// directions page, otherwise he has to pay the previous bill
+	if (customer1.checkPreviousBill() == true) {
+		var location1 = new Location(47.33379404, 52.42461880);
+		var car1 = new Car("A1B2", "Audi", "1998", "small", 0.4, 5, "Bob",
+				"picture", location1, "description");
+
+		var location = car1.location;
+		// Set the car status to reserved
+		car1.status = "reserved";
+
+		// Open directionToCar page with directionToCar
+		window.location = 'directionToCar.html';
+	} else {
+		window.location = 'payBill.html';
+	}
+}
