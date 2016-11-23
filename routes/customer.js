194308f693e9c51a70f9c09ab3a46a18d88bf543
hendleje <jenny2912@gmx.de>
2016-11-23 16:26:11 -0330

    Implemented checkBill for customer, creation of rental agreement, calculate costs when car is returned

--- routes/customer.js
+++ routes/customer.js
@@ -9,13 +9,16 @@ function Person(firstname, lastname, email, address) {
 	 * TODO create proper ID automatically
 	 */
 	this.id = 1;
+	this.unpaidbills = new Array();
+	this.paidbills = new Array();
 }
 
 // ------------ Customer ------------
 // Creates a customer
 function Customer(firstname, lastname, email, address, location) {
 	Person.call(this, firstname, lastname, email, address);
-	this.status = cusstatus.SUSPENDED;
+	// TODO: Once confirmation mail is sent, set status initially to suspended
+	this.status = cusstatus.ACTIVE;
 	this.sendconfirmationemail();
 	this.location = location;
 }
@@ -33,27 +36,35 @@ Customer.prototype = Object.create(Person.prototype);
 
 // Check, if the previous bill of the customer was paid
 Customer.prototype.checkpreviousbill = function(){
-	if (this.status == cusstatus.SUSPENDED) {
+	if (this.status == cusstatus.SUSPENDED || this.unpaidbills.length > 0) {
+		console.log("UNpaidbills length: " + this.unpaidbills.length);
 		return false;
 	}
 	else {
-		/**
-		 * TODO Implement check of previous bill
-		 */
 		return true;
 	}
 }
 
-// Set the customer's status to active again, if all the bills are payed
-Customer.prototype.billpayed = function(bill) {
-	/**
-	 * TODO Check, if any other bills are not payed, if not, set the status to active again
-	 */
+// Set the customer's status to active again, if all the bills are paid
+Customer.prototype.billpaid = function(bill) {
+	// Remove bill from unpaid bills list
+	var index = this.unpaidbills.indexOf(bill);
+	if (index > -1) {
+	    this.unpaidbills.splice(index, 1);
+	}
+	
+	// Add bill to paid bills list
+	this.paidbills.push(bill);
+	
+	// If all bills are paid, set status to active again
+	if (this.unpaidbills.length == 0) {
+		this.status = cusstatus.ACTIVE;
+	}
 }
 
 // Add a new bill to the current list of bills
 Customer.prototype.addbill = function(bill) {
-	this.bills.add(bill);
+	this.unpaidbills.push(bill);
 }
 
 // Send a email so the customer can confirm his/her mailaddress
@@ -70,3 +81,28 @@ Customer.prototype.customerverified = function() {
 	console.log("Customer status: " + this.status);
 	return "Customer status: " + this.status;
 }
+
+//Tests
+//var customer1 = new Customer("Jenny", "Hendler", "bla", "bla");
+//console.log(customer1.status);
+//console.log(customer1.checkpreviousbill());
+//customer1.addbill(1);
+//customer1.addbill(2);
+//customer1.addbill(3);
+//console.log("All " + customer1.unpaidbills.length + " bills added:");
+//for (var i=0; i<customer1.unpaidbills.length; i++) {
+//	console.log(customer1.unpaidbills[i]);
+//}
+//console.log(customer1.checkpreviousbill());
+//customer1.billpaid(2);
+//customer1.billpaid(1);
+//customer1.billpaid(3);
+//console.log("Bill 2 paid:");
+//for (var i=0; i<customer1.unpaidbills.length; i++) {
+//	console.log("unpaid: " + customer1.unpaidbills[i]);
+//}
+//for (var i=0; i<customer1.paidbills.length; i++) {
+//	console.log("paid: " + customer1.paidbills[i]);
+//}
+//console.log(customer1.status);
+//console.log(customer1.checkpreviousbill());
--- routes/rentalagreement.js
+++ routes/rentalagreement.js
@@ -1,33 +1,78 @@
 // ------------ Rental Agreement ------------
 // Creates a rental agreement
-function Rentalagreement(plate, brand, model, color, year, category, status, kmcost, timecost, creator, picture, location, description, carclass) {
+function Rentalagreement(customer, car) {
 	this.starttime = this.getcurrenttime();
-	this.model = model;
 	this.car = car;
 	this.customer = customer;
 }
 
 // When returning the car all remaining information is calculated
-Rentalagreement.prototype.carreturned = function(kmdriven) {
+Rentalagreement.prototype.carreturned = function(kmdriven, kmcost, timecost) {
+	console.log("Car returned");
+	// Save km driven
 	this.kmdriven = kmdriven;
+	
+	// Save endtime
 	this.endtime = this.getcurrenttime();
-	// TODO calculate hours driven properly;
-	this.timedriven = endtime - starttime;
-	this.costs = this.calculatecosts(kmdriven, timedriven);
+	
+	// Calculate and save time driven
+	this.timedriven = Math.round((this.endtime - this.starttime)/1000/60/60*100) /100;
+	
+	// Calculate costs
+	this.cost = this.calculatecosts(kmcost, timecost);
+	this.sum = this.cost[0] + this.cost[1];
 }
 
 //Calculate costs for km and hours driven
-Rentalagreement.prototype.calculatecosts = function(kmdriven, hoursdriven) {
-	var kmcostcur = kmdriven * this.kmcost;
-	var timecostcur = hoursdriven * this.timecost;
+Rentalagreement.prototype.calculatecosts = function(kmcost, timecost) {
+	var kmcostcur = this.kmdriven * kmcost;
+	var timecostcur = this.timedriven * timecost;
 	var costs = [kmcostcur, timecostcur];
 	return costs;
 }
 
 //Gets the current time
 Rentalagreement.prototype.getcurrenttime = function() {
-	// TODO Implement how to get current time
-	return currenttime;
+	var currentdate = new Date(); 
+//	var datetime = "Last Sync: " + currentdate.getDate() + "/"
+//	                + (currentdate.getMonth()+1)  + "/" 
+//	                + currentdate.getFullYear() + " @ "  
+//	                + currentdate.getHours() + ":"  
+//	                + currentdate.getMinutes() + ":" 
+//	                + currentdate.getSeconds();
+//	console.log(datetime);
+	return currentdate;
+}
+
+// Print date
+Rentalagreement.prototype.printdate = function(date) {
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	if (month < 10) {
+		month = "0" + month;
+	}
+	var year = date.getFullYear();
+	var hours = date.getHours();
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	var min = date.getMinutes();
+	if (min < 10) {
+		min = "0" + min;
+	}
+	var sec = date.getSeconds();
+	if (sec < 10) {
+		sec = "0" + sec;
+	}
+	var nicedate = "" + hours + ":" + min + ":" + sec + " (" + day + "/" + month + "/" + year + ")";
+	return nicedate;
 }
 
 module.exports = Rentalagreement;
+
+// Test
+//ra = new Rentalagreement(1, 2);
+//console.log(ra.starttime);
+//ra.carreturned(10);
+//var nice = ra.printdate(ra.starttime);
+//console.log(nice);
